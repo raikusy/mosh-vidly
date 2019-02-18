d183aec59eeeb24f3ba2363aacba4ba98f4d6636
Rakibul Hasan <xenax.rakibul@gmail.com>
2019-02-18 19:06:11 +0600

    Production

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./startup/db')();
 require('./startup/routes')(app);
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 // Listen to port 3000
 const PORT = process.env.PORT || 3000;
